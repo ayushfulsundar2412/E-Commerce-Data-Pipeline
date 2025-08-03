280c1df62539c462583544d52fa51914ea4301dd
ayushfulsundar <fulsundar.a@northeastern.edu>
2025-08-03 11:30:50 +0530

    Added dbt transformations: staged raw tables (orders, customers, shipments) and created order_status mart with delivery delay logic

---
+++ dbt_ecommerce/target/run/ecommerce_project/models/staging/stg_customers.sql
@@ -0,0 +1,10 @@
+
+  create or replace   view ECOMMERCE_DB.analytics.stg_customers
+  
+  
+  
+  
+  as (
+    select * from raw.customers
+  );
+
---
+++ dbt_ecommerce/target/run/ecommerce_project/models/staging/stg_orders.sql
@@ -0,0 +1,10 @@
+
+  create or replace   view ECOMMERCE_DB.analytics.stg_orders
+  
+  
+  
+  
+  as (
+    select * from raw.orders
+  );
+
